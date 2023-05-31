fd11b0376a6539033a1c03ab7e36821d0181d7e5
Riccardo Ventura <r.ventura@outlook.it>
2023-05-31 09:51:15 +0200

    update

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+function Navbar(){
+    return (
+<nav className="navbar bg-primary" data-bs-theme="dark">
+<div className="container-fluid">
+    <span className="navbar-brand mb-0 h1">Sushi!</span>
+  </div>
+</nav>
+    );
+}
+
+export default Navbar;
+
+
